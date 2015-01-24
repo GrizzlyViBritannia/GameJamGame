d44bbb5e5e3cca30b9311c92e04bd311c9eeab25
SpoonFed <jogden16761@gmail.com>
2015-01-24 01:29:25 +0000

    added classes and stuff

---
+++ GameJamGame/joels Work/Board.cs
@@ -0,0 +1,39 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace GameJamGame.joels_Work
+{
+    class Board
+    {
+        Texture2D backGround;
+
+
+        public void logic(GameTime gameTime)
+        {
+
+
+
+
+        }
+
+
+        public void draw(SpriteBatch SB)
+        {
+
+
+        }
+
+
+        
+
+
+    }
+}
---
+++ GameJamGame/joels Work/Level.cs
@@ -0,0 +1,34 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace GameJamGame.joels_Work
+{
+    class Level 
+    {
+        /*
+         * this class is going to be used to simply save the data in 
+         * memory. each Level will be created in code at initilisation
+        */
+        Texture2D backGround;
+        Rectangle drawRect;
+        List<GameObject> objectList;
+
+
+
+        public Level(Texture2D backGround, Rectangle drawRect, List<GameObject> gameObject)
+        {
+            this.backGround = backGround;
+            this.drawRect = drawRect;
+        }
+               
+
+    }
+}

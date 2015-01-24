bd735d49a8bb409f717948e82d4da5424900fd59
thesep42 <thesep42@googlemail.com>
2015-01-24 03:04:33 +0000

    Put it in.

--- GameJamGame/joels Work/Player.cs
+++ GameJamGame/joels Work/Player.cs
@@ -13,36 +13,105 @@ using Microsoft.Xna.Framework.GamerServices;
 
 namespace GameJamGame.joels_Work
 {
-    class Player : GameObject
-    {
+	class Player : GameObject
+	{
 
-        enum State
-        {
-            Walking,
-            Jumping
-        }
-        State currentState = State.Walking;
-
-        Vector2 mDirection = Vector2.Zero;
-        Vector2 mSpeed = Vector2.Zero;
-
-        KeyboardState mPreviousKeyboardState;
-
-        public void Update(GameTime theGameTime)
-        {
-            //KeyboardState aCurrentKeyboardState = Keyboard.GetState();
-
-            //UpdateMovement(aCurrentKeyboardState);
-
-            //mPreviousKeyboardState = aCurrentKeyboardState;
-
-            //base.Update(theGameTime, mSpeed, mDirection);
-        }
-
-        private void UpdateMovement(KeyboardState currentKeyboardState)
-        {
+		const int WALKING_SPEED = 160;
+		const int MOVE_LEFT = -1;
+		const int MOVE_RIGHT = 1;
+		const int MOVE_DOWN = -1;
+		const int MOVE_UP = 1;
 
 
-        }
-    }
+		enum State
+		{
+			Walking,
+			Jumping,
+			Falling
+		}
+		State currentState = State.Walking;
+
+		public Vector2 Position = new Vector2(0, 0);
+
+		Vector2 mDirection = Vector2.Zero;
+		Vector2 mSpeed = Vector2.Zero;
+		Vector2 jumpStartingPosition = Vector2.Zero;
+
+		KeyboardState mPreviousKeyboardState;
+
+		public void update(GameTime theGameTime)
+		{
+			KeyboardState currentKeyboardState = Keyboard.GetState();
+
+			updateMovement(currentKeyboardState);
+
+			mPreviousKeyboardState = currentKeyboardState;
+		}
+
+		private void updateMovement(KeyboardState currentKeyboardState)
+		{
+			if (currentState == State.Walking)
+			{
+				mSpeed = Vector2.Zero;
+				mDirection = Vector2.Zero;
+
+				if (currentKeyboardState.IsKeyDown(Keys.Left) == true || currentKeyboardState.IsKeyDown(Keys.A) == true)
+				{
+					mSpeed.X = WALKING_SPEED;
+					mDirection.X = MOVE_LEFT;
+				}
+				else if (currentKeyboardState.IsKeyDown(Keys.Right) == true || currentKeyboardState.IsKeyDown(Keys.D) == true)
+				{
+					mSpeed.X = WALKING_SPEED;
+					mDirection.X = MOVE_RIGHT;
+				}
+			}
+		}
+
+		private void updateJump(KeyboardState currentKeyboardState)
+		{
+			if (currentState == State.Walking)
+			{
+				if (jumpInit(currentKeyboardState) == true)
+				{
+					jump();
+				}
+			}
+		}
+
+		private bool jumpInit(KeyboardState currentKeyboardState)
+		{
+			bool doJump = false;
+
+			if (currentKeyboardState.IsKeyDown(Keys.Space) == true &&
+				   mPreviousKeyboardState.IsKeyDown(Keys.Space) == false)
+			{
+				doJump = true;
+			}
+			else if (currentKeyboardState.IsKeyDown(Keys.W) == true &&
+				 mPreviousKeyboardState.IsKeyDown(Keys.W) == false)
+			{
+				doJump = true;
+			}
+			else if (currentKeyboardState.IsKeyDown(Keys.Up) == true &&
+				 mPreviousKeyboardState.IsKeyDown(Keys.Up) == false)
+			{
+				doJump = true;
+			}
+
+			return doJump;
+		}
+
+		private void jump()
+		{
+			if (currentState != State.Jumping)
+			{
+				currentState = State.Jumping;
+				jumpStartingPosition = Position;
+				mDirection.Y = MOVE_UP;
+				mSpeed = new Vector2(WALKING_SPEED, WALKING_SPEED);
+			}
+		}
+
+	}
 }

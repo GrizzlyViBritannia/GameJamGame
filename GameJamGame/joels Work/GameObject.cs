9cd8a803dff3f3edc9c651f3ed0597e6d7462439
SpoonFed <jogden16761@gmail.com>
2015-01-24 00:52:22 +0000

    added classes
    added object and player class

---
+++ GameJamGame/joels Work/GameObject.cs
@@ -0,0 +1,54 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+
+namespace GameJamGame.joels_Work
+{
+    class GameObject
+    {
+        // logic variables
+        protected Vector2 centerPosition;
+        protected Rectangle collisonRect;
+        protected bool movable;
+
+        // draw variable
+        protected Rectangle drawRect;
+        protected Color colour;
+        protected Texture2D sprite;
+
+        // private functions:
+        
+
+
+        // public functions:
+        public void moveObject(Vector2 input)
+        {
+            this.centerPosition += input;
+        }
+
+
+        // get-set functions:
+        public bool isMovable()
+        {
+            return this.movable;
+        }
+
+
+        /*
+         * Chris, produce a function to go in the Board class (not yet created).
+         * the functions will take two objects, check if they are colliding, and 
+         * fix the collision via the "moveObject(Vector2)" function.
+         * this function takes a Vector2 variable and will move the object by 
+         * said vector
+         */
+
+    }
+}
---
+++ GameJamGame/joels Work/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameJamGame.joels_Work
+{
+    class Player : GameObject
+    {
+        
+        private void movement()
+        {
+            // sams movement function
+
+        }
+    }
+}
